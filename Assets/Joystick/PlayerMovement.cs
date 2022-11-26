512d82fae2210173db146da7de3568be26e6139f
ahmtkrbgli <ahmtkrbgli@gmail.com>
2022-11-26 16:48:40 +0300

    v 0.33
    -joystick
    -playermovemnt

---
+++ Assets/Joystick/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private new Rigidbody rigidbody;
+    [SerializeField] private FixedJoystick joystick;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float movementSpeed;
+
+    private void Update()
+    {
+        Movement();  
+    }
+
+    void Movement()
+    {
+        rigidbody.velocity = new Vector3(joystick.Horizontal * movementSpeed, rigidbody.velocity.y, joystick.Vertical * movementSpeed);
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+        }
+    }
+}
